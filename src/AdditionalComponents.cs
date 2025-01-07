22f8e984f26b19143a4026f8a6331fa1eba73b29
David Spörri <davepermen@hotmail.com>
2025-01-07 11:39:48 +0100

    lets go

---
+++ src/AdditionalComponents.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace Conesoft.PwaGenerator;
+
+public class AdditionalComponents
+{
+    public static Assembly Assembly => typeof(AdditionalComponents).Assembly;
+}
