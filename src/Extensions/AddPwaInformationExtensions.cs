32cab1a1d1244edf4033bf916e797db38083d6aa
David Spörri <davepermen@hotmail.com>
2025-02-26 10:24:45 +0100

    pwa now extensible through wwwroot/site.webmanifest

--- src/Extensions/AddPwaInformationExtensions.cs
+++ src/Extensions/AddPwaInformationExtensions.cs
@@ -1,3 +1,4 @@
+using Conesoft.Tools;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
@@ -5,6 +6,9 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.ComponentModel;
 using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace Conesoft.PwaGenerator;
 
@@ -12,17 +16,20 @@ namespace Conesoft.PwaGenerator;
 [EditorBrowsable(EditorBrowsableState.Never)]
 public static class AddPwaInformationExtensions
 {
+    private static readonly string[] bots = ["GPTBot", "ChatGPT-User", "Google-Extended", "PerplexityBot", "Amazonbot", "ClaudeBot", "Omgilibot", "FacebookBot", "Applebot", "anthropic-ai", "Bytespider", "Claude-Web", "Diffbot", "ImagesiftBot", "Omgilibot", "Omgili", "YouBot"];
+
     public static WebApplication MapPwaInformationFromAppSettings(this WebApplication app)
     {
-        var configuration = app.Services.GetRequiredService<IConfiguration>();
-        var name = configuration.GetValue<string>("pwa:name") ?? throw new Exception("appsettings.json configuration wrong, pwa:name not found");
-        var url = configuration.GetValue<string>("pwa:url") ?? throw new Exception("appsettings.json configuration wrong, pwa:url not found");
-        var description = configuration.GetValue<string>("pwa:description") ?? throw new Exception("appsettings.json configuration wrong, pwa:description not found");
+        var c = app.Services.GetRequiredService<IConfiguration>();
+        var name = c.GetValue<string>("pwa:name") ?? throw new Exception("appsettings.json configuration wrong, pwa:name not found");
+        var url = c.GetValue<string>("pwa:url") ?? throw new Exception("appsettings.json configuration wrong, pwa:url not found");
+        var description = c.GetValue<string>("pwa:description") ?? throw new Exception("appsettings.json configuration wrong, pwa:description not found");
+        var configuration = Safe.Try(() => File.ReadAllText(@"wwwroot\site.webmanifest")) ?? "{}";
 
-        return app.MapPwaInformation(name, url, description);
+        return app.MapPwaInformation(name, url, description, configuration);
     }
 
-    public static WebApplication MapPwaInformation(this WebApplication app, string name, string url, string description)
+    public static WebApplication MapPwaInformation(this WebApplication app, string name, string url, string description, string configuration)
     {
         var svg = File.Exists($"wwwroot/meta/favicon.svg");
         var png = File.Exists($"wwwroot/meta/favicon.png");
@@ -84,8 +91,13 @@ public static class AddPwaInformationExtensions
             display_override = new[] { "window-controls-overlay" }
         };
 
-        app.MapGet("/pwa/site.webmanifest", () => Results.Json(json, contentType: "application/manifest+json"));
-        app.MapGet("/robots.txt", () => "User-agent: GPTBot\r\nDisallow: /\r\n");
+        var main = JsonSerializer.SerializeToNode(json)!;
+        var extensions = JsonNode.Parse(configuration)!;
+
+        var output = main.Merge(extensions);
+
+        app.MapGet("/pwa/site.webmanifest", () => Results.Json(output, contentType: "application/manifest+json"));
+        app.MapGet("/robots.txt", () => string.Join("\r\n", bots.Select(bot => $"User-agent: {bot}\r\nDisallow: /")));
         return app;
     }
 }
